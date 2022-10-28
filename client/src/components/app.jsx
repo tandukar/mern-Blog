c829dbdd0fdcee3d00c51ac42ee9cb489e396a79
tandukar <84461389+tandukar@users.noreply.github.com>
2022-10-28 22:19:11 +0545

    fetching users

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -4,18 +4,25 @@ import "./app.css";
 import Navbar from "./navbar";
 import Signup from "./signup";
 import Login from "./login";
+import Fetch from "./fetch";
 
 
 const App = () => {
-var registered =true;
+var registered =false;
+var loggedIn =false;
 
   return <div>
     <Navbar/>
-    {registered ? <div> <h3>Login</h3><Login/></div>: <div><h3>Sign up</h3><Signup/></div>  }
+    
+    {loggedIn ? <Fetch/> : <div>{registered ? <div> <h3>Login</h3><Login/></div>: <div><h3>Sign up</h3><Signup/></div>  }</div>};
   
      
   
   </div>
 };
+
+
+
+
 
 export default App;
