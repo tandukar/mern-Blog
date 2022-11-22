7988fc4e5fcf64a3336dd2cac4923cb0be56a293
tandukar <84461389+tandukar@users.noreply.github.com>
2022-11-22 20:36:15 +0545

    routes

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./app.css";
 
+
+import Dashboard from "./components/pages/dashboard/Dashboard";
+import Login from "./components/pages/login/Login";
+import Register from "./components/pages/register/Register";
 
 
 const App = () => {
-
   return (
-  <></>
-
-  )
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Dashboard/>}></Route>
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path='/regsiter' element={<Register/>}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 };
-
-
-
-
 
 export default App;
