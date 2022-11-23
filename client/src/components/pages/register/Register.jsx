88e4e0bae097c4ca22e51e5f72333ef94c060b35
tandukar <84461389+tandukar@users.noreply.github.com>
2022-11-23 15:02:00 +0545

    register frontend only (no databse)

--- client/src/components/pages/register/Register.jsx
+++ client/src/components/pages/register/Register.jsx
@@ -1,13 +1,105 @@
-import React, { Component } from 'react';
+import { useState } from "react";
+import '../index.css';
+
+//import index.css
+
+function Register() {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    age: "",
+    email: "",
+    password: "",
+  });
+
+  const { firstname, lastname, age, email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState)=>({   //object ho bhane function ko curly braces ma () halne
+      ...prevState,
+      [e.target.name]: e.target.value,
+
+    }));
+  };
 
 
-function Register(){
-    return(
-        <>
-           <div>
-            <h1>Register</h1>
-        </div></>
-    );
+  const onSubmit = (e) => {
+    e.preventDefault();
+  
+    console.log( firstname, lastname,age, email, password);
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h2>Register</h2>
+        <p>Create an Account</p>
+      </section>
+
+      <section className="form">
+        <form  onSubmit={onSubmit}> 
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="firstname"
+              name="firstname"
+              value={firstname}
+              placeholder="Enter your name"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="lastname"
+              name="lastname"
+              value={lastname}
+              placeholder="Enter your name"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="number"
+              className="form-control"
+              id="age"
+              name="age"
+              value={age}
+              placeholder="Enter your Age"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter  password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
 export default Register;
